81542f69b7f3af67c0082f4994475b28807937f6
WajahatQadri <muhammadwajahathussainchishti.com>
2026-04-13 14:52:37 +0500

    code

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,58 +1,78 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({
+const doctorSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User", 
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    role : {
-        type : String,
-        default : "USER"
+    role: {
+      type: String,
+      default: "USER",
     },
-    specialization : {
-        type : String,
-        required : true
+    specialization: {
+      type: String,
+      required: true,
     },
-    contact : {
+    contact: {
+      type: String,
+      required: true,
+      match: [
+      /^(\d{4}[-\s]\d{7}|\+\d{1,3}[-\s]\d{3}[-\s]\d{7})$/, 
+      'Invalid format. Use xxxx-xxxxxxx or +XX-XXX-XXXXXXX'
+    ],
+      trim: true,
+    },
+    fees: {
+      type: Number,
+      required: true,
+    },
+    days: [
+      {
         type: String,
         required: true,
-        match: [/^\d{10,15}$/, 'Please fill a valid phone number']
-    },
-    fees : {
-        type : Number,
-        required : true
-    },
-    days : [{
-        type : String,
-        required : true
-    }],
-    timing : [{
-        type : String,
-        required : true
-    }],
+      },
+    ],
+    timing: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
     isApproved: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-    patients: [{
+    patients: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Patient"
-    }],
+        ref: "Patient",
+      },
+    ],
     experience: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     hospital: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     address: {
-        type: String,
-    }
-},{
-    timestamps : true
-})
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+doctorSchema.pre('save', function(next) {
+  if (this.contact) {
+    this.contact = this.contact.replace(/-/g, ' ');
+  }
+  next();
+});
 
 const Doctor = mongoose.model("Doctor", doctorSchema);
-export default Doctor
+export default Doctor;
